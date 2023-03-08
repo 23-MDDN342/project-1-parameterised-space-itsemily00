0d6869ee75c11da1241f7ff4d4f8ea01134217ea
Bex Campbell <zixinxu0080@gmail.com>
2023-03-09 11:01:37 +1300

    making changes

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,34 +1,84 @@
-var x=300;
-var y=300;
-var a=100;
-var b=100;
-
-function draw_one_frame() {
+/* 
+   getNoiseValue arguments:
+     x: current grid location across
+     y: current grid location down
+     loop: can be any value from 0-1 and will loop
+     name: the "name" of the lookup table. probably change this each time.
+     min/max: the minimum and maximum of the value to return
+     smoothness: 1 means elements are not related. larger numbers cause groupings.
+*/
+function draw_one_frame(cur_frac) {
   //background(255);
-  x+=2;
-  y+=2;
-	a-=2;
-	b-=2;
-	strokeWeight(1);
-  translate(width/2, height/2);
-  for(var i=0;i<15;i++){
-	  for(var k=0;k<20;k++){
-		stroke(255,255,255);
-    rotate(PI / 12.0);
-	  fill(255,255-i*10,255-k*10);
-  	line(a%100,b%100,x%300,y%300);
-	  ellipse((x+i*20)%width,(y+k*20)%height,i+4,i+4);
-		drawtriangle((a-i*20)%width,(b-k*20)%height,k/8);
-		rect(x%width, y%height, k+10, k+10);
-		fill(0,i*10,255-k*10);
-		ellipse((x-i*20)%width,(y-k*20)%height,i+3,i+3);
-		rotate(PI / 24.0);
-		fill(255-(i+k)*5,(i+k)*7,i*20);
-		drawtriangle((a+i*20)%width,(b+k*20)%height,k/8);
-		rect(a%width, b%height, k+5, k+5);
-		drawflower(k,x);
-	  }
+  let backgroundColor = color("##fffff")
+  fill(backgroundColor)
+  rect(0,0, width, height)
+  //////////////////////////////////////set up////////
+  let spacingSize = width / 20;
+  
+  let noiseColor;
+let noiseyColor; 
+// let backupColor = color("#D34F73") // blush
+
+let from = color("#e64937");
+let to = color("#ffffff");
+let interA = lerpColor(from, to, 0.8);
+
+let backupColor = color("#f5c022") // yellow
+let DetailColor = color("#e64937") //red
+//let DetailColor = interA;
+let mainColor = color("#68abed") //blue
+
+
+
+  for(let i = 0; i < width / spacingSize; i++){
+	for(let down = 0; down +1 < height / spacingSize; down++){
+		fill(0);
+		ellipse(spacingSize*i, spacingSize*down, 4); //
+		//line(width/2, spacingSize*i, width/2, frameCount); 
+		noiseColor = getNoiseValue(spacingSize*i,spacingSize*down, 1, "noiseColor",0,1, 180 )
+		noiseyLerp = lerpColor(mainColor,backupColor,noiseColor)  // https://p5js.org/reference/#/p5/lerpColor
+		fill(noiseyLerp)
+		strokeWeight(1);
+		rect(spacingSize*i, spacingSize*down, spacingSize, spacingSize);
+
+
+		noiseColor2 = getNoiseValue(spacingSize*i, spacingSize*down, 1, "noiseColor2", 0, 1, 200);
+		noiseLerp2 = lerpColor(from, to, noiseColor2);
+
+	
+		if(cur_frac > 0.3 && noiseColor < 0.3){
+			fill(225)
+			moveXMap = map(cur_frac,0.3, 1, spacingSize*(i+1), spacingSize*(i))
+			fill(noiseLerp2);
+			rect(moveXMap,spacingSize*down,spacingSize) 
+			
+		}
+		
+		
+
+	}
+	
   }
+	
+
+//   for(var i=0;i<15;i++){
+// 	  for(var k=0;k<20;k++){
+// 		stroke(255,255,255);
+//     rotate(PI / 12.0);
+// 	  fill(255,255-i*10,255-k*10);
+//   	line(a%100,b%100,x%300,y%300);
+// 	  ellipse((x+i*20)%width,(y+k*20)%height,i+4,i+4);
+// 		drawtriangle((a-i*20)%width,(b-k*20)%height,k/8);
+// 		rect(x%width, y%height, k+10, k+10);
+// 		fill(0,i*10,255-k*10);
+// 		ellipse((x-i*20)%width,(y-k*20)%height,i+3,i+3);
+// 		rotate(PI / 24.0);
+// 		fill(255-(i+k)*5,(i+k)*7,i*20);
+// 		drawtriangle((a+i*20)%width,(b+k*20)%height,k/8);
+// 		rect(a%width, b%height, k+5, k+5);
+// 		drawflower(k,x);
+// 	  }
+//   }
 
 }
 
