f06ef958ea71337b6ab119a9dfd4a8c880b5ca33
Bex Campbell <zixinxu0080@gmail.com>
2023-03-12 15:50:51 +1300

    update sketch
    updating sketch image of my initial ideas. Late updating as the sketch was saved in the wrong folder.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300737&assignment_repo_type=AssignmentRepo)
 ### 2023 MDDN342 Project 1: Parameterised Space
 
-
-The lines will grow and extend from points, forming the lines and squares. at the same time maybe, the colours will occur by fliping themselves down? the lines are like strings. then the image tilts and everything collapse, colours slide out of the canvas, the lines are shrink into points. then loop
+Initial idea
+Mondrian inspired. The lines will grow and extend from points, forming the lines and squares. at the same time maybe, the colours will occur by fliping themselves down? the lines are like strings. then the image tilts and everything collapse, colours slide out of the canvas, the lines are shrink into points. then loop.
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -7,94 +7,57 @@
      min/max: the minimum and maximum of the value to return
      smoothness: 1 means elements are not related. larger numbers cause groupings.
 */
-function draw_one_frame(cur_frac) {
-  //background(255);
-  let backgroundColor = color("##fffff")
-  fill(backgroundColor)
-  rect(0,0, width, height)
-  //////////////////////////////////////set up////////
-  let spacingSize = width / 20;
-  
-  let noiseColor;
-let noiseyColor; 
-// let backupColor = color("#D34F73") // blush
+let lineX1 = 200;
+let lineY1 = 200;
+let lineY2 = 400;
+let lineLength = 400;
+//let lineX2 = lineX1+frameCount*5;
 
-let from = color("#e64937");
-let to = color("#ffffff");
-let interA = lerpColor(from, to, 0.8);
-
-let backupColor = color("#f5c022") // yellow
-let DetailColor = color("#e64937") //red
-//let DetailColor = interA;
-let mainColor = color("#68abed") //blue
-
-
-
-  for(let i = 0; i < width / spacingSize; i++){
-	for(let down = 0; down +1 < height / spacingSize; down++){
-		fill(0);
-		ellipse(spacingSize*i, spacingSize*down, 4); //
-		//line(width/2, spacingSize*i, width/2, frameCount); 
-		noiseColor = getNoiseValue(spacingSize*i,spacingSize*down, 1, "noiseColor",0,1, 180 )
-		noiseyLerp = lerpColor(mainColor,backupColor,noiseColor)  // https://p5js.org/reference/#/p5/lerpColor
-		fill(noiseyLerp)
-		strokeWeight(1);
-		rect(spacingSize*i, spacingSize*down, spacingSize, spacingSize);
-
-
-		noiseColor2 = getNoiseValue(spacingSize*i, spacingSize*down, 1, "noiseColor2", 0, 1, 200);
-		noiseLerp2 = lerpColor(from, to, noiseColor2);
-
-	
-		if(cur_frac > 0.3 && noiseColor < 0.3){
-			fill(225)
-			moveXMap = map(cur_frac,0.3, 1, spacingSize*(i+1), spacingSize*(i))
-			fill(noiseLerp2);
-			rect(moveXMap,spacingSize*down,spacingSize) 
-			
-		}
-		
-		
-
-	}
-	
-  }
+function draw_one_frame(){
+	let backgroundColor = color("#fffff");
+	fill(backgroundColor);
+	rect(0, 0, width, height);
+//////////SET UP///////////////
 	
 
-//   for(var i=0;i<15;i++){
-// 	  for(var k=0;k<20;k++){
-// 		stroke(255,255,255);
-//     rotate(PI / 12.0);
-// 	  fill(255,255-i*10,255-k*10);
-//   	line(a%100,b%100,x%300,y%300);
-// 	  ellipse((x+i*20)%width,(y+k*20)%height,i+4,i+4);
-// 		drawtriangle((a-i*20)%width,(b-k*20)%height,k/8);
-// 		rect(x%width, y%height, k+10, k+10);
-// 		fill(0,i*10,255-k*10);
-// 		ellipse((x-i*20)%width,(y-k*20)%height,i+3,i+3);
-// 		rotate(PI / 24.0);
-// 		fill(255-(i+k)*5,(i+k)*7,i*20);
-// 		drawtriangle((a+i*20)%width,(b+k*20)%height,k/8);
-// 		rect(a%width, b%height, k+5, k+5);
-// 		drawflower(k,x);
-// 	  }
-//   }
+	push();
+	strokeWeight(3);
+	horizontal_lines(80, 250, 250);
+	horizontal_lines(450, 400, 450);
+	vertical_lines(800, 300, 150);
+	pop();
+
+	push();
+	strokeWeight(5);
+	horizontal_lines(200, 100, 500);
+	horizontal_lines(500, 150, 300);
+	vertical_lines(450, 200, 300);
+	vertical_lines(680, 30, 370);
+	pop();
+
+	push();
+	strokeWeight(8.5);
+	horizontal_lines(330, 200, 200);
+	horizontal_lines(150, 400, 180);
+	vertical_lines(330, 50, 260);
+	pop();
+
+	
+	
 
 }
 
-function drawtriangle(x,y,r){
-	triangle(x, y, x+7*r, y-13.75*r, x+14*r, y);
+function horizontal_lines(lineX1, lineY1, lineLength){
+	stroke(0);
+	strokeCap(SQUARE); //https://p5js.org/reference/#/p5/strokeCap
+	line(lineX1, lineY1, lineX1+lineLength, lineY1);
 }
 
-function drawflower(i,k){
-		if(i%2==1){
-			fill(255,(k*0.4)%255,30);
-			stroke(k%255,255,0);
-			arc(0,0,150+i+150*sin(k*PI/24),150,0,PI / 40);
-		}
-		else{
-			fill(k%255,0,255);
-			stroke(0,(k*0.4)%255,255);
-			arc(0,0,(100+100*cos(k*PI/24))%255,50,0,PI / 20);
-		}
+function vertical_lines(lineX1, lineY1, lineLength){
+	stroke(0);
+	strokeCap(SQUARE);
+	line(lineX1, lineY1, lineX1, lineY1+lineLength);
 }
+
+
+
