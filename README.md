404ee558f70970342d5b78ec5242766ced2f0b40
Bex Campbell <zixinxu0080@gmail.com>
2023-03-14 11:15:15 +1300

    making color blocks move
    experimenting the movement of the color blocks

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 
 Initial idea
 Mondrian inspired. The lines will grow and extend from points, forming the lines and squares. at the same time maybe, the colours will occur by fliping themselves down? the lines are like strings. then the image tilts and everything collapse, colours slide out of the canvas, the lines are shrink into points. then loop.
+
+Iteration 12/03
+Didn't like how my initial idea loops, it was not satisifying enough. 
+I will draw the black lines/grids as the background, so instead of having the lines growing from dots, they will stay in the form of lines all the time. The looping part will be the colcours.The colour geometric shapes will slide into the grid instead of "flipping". 
+
+Iteration 13/03
+Will cancel the sliding motion. Instead, make the color blocks look like they are "breathing": the color blocks will change in size proportionally (they go large - small - large - small...). 
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -13,13 +13,42 @@ let lineY2 = 400;
 let lineLength = 400;
 //let lineX2 = lineX1+frameCount*5;
 
-function draw_one_frame(){
+
+function draw_one_frame(cur_frac){
 	let backgroundColor = color("#fffff");
 	fill(backgroundColor);
 	rect(0, 0, width, height);
-//////////SET UP///////////////
-	
+///////////////SET UP///////////////
 
+
+/////DRAW THE COLOR BLOCKS/////
+let mainRed = color("#ed1e1a"); // red
+mainRed.setAlpha(180); //https://p5js.org/reference/#/p5.Color/setAlpha
+let backupRed = color("#bf0f0b"); //scarlet
+backupRed.setAlpha(180);
+
+let noiseRed;
+let blockSize = 10;
+let minSize = 1;
+let maxSize = 100;
+let rectX = 0;
+let rectY = 0;
+
+
+fill(mainRed);
+noStroke();
+rectMode(CENTER);
+let scaledSize = map(cur_frac, 0, 1, 0.1, 5);
+push();
+translate(265, 150);
+scale(scaledSize, scaledSize);
+rect(0, 0, 130, 100);
+pop(); 
+blockSize = (blockSize % maxSize) + 1; //chatGPT
+
+
+
+/////DRAW THE GRID/////
 	push();
 	strokeWeight(3);
 	horizontal_lines(80, 250, 250);
@@ -31,6 +60,8 @@ function draw_one_frame(){
 	strokeWeight(5);
 	horizontal_lines(200, 100, 500);
 	horizontal_lines(500, 150, 300);
+	horizontal_lines(600, 350, 220);
+	vertical_lines(200, 80, 350);
 	vertical_lines(450, 200, 300);
 	vertical_lines(680, 30, 370);
 	pop();
@@ -38,12 +69,13 @@ function draw_one_frame(){
 	push();
 	strokeWeight(8.5);
 	horizontal_lines(330, 200, 200);
+	horizontal_lines(450, 310, 170);
 	horizontal_lines(150, 400, 180);
-	vertical_lines(330, 50, 260);
+	vertical_lines(330, 50, 400);
 	pop();
 
-	
-	
+
+
 
 }
 
@@ -59,5 +91,42 @@ function vertical_lines(lineX1, lineY1, lineLength){
 	line(lineX1, lineY1, lineX1, lineY1+lineLength);
 }
 
+function redBlocks(){
+	// let orbSize = width / 20
+	// let spacingSize = width / 19;
+	// let mainRed = color("#ed1e1a"); // red
+	// mainRed.setAlpha(180); //https://p5js.org/reference/#/p5.Color/setAlpha
+	// let backupRed = color("#bf0f0b"); //scarlet
+	// backupRed.setAlpha(180);
+	
+	// let noiseRed;
+	// let sizeMap;
+	// let mainColor = color("#3F292B") // brown
+	
+	//noiseRed = getNoiseValue(spacingSize*accross,spacingSize*down, 0.8, "noiseRed",0,1, 200 )
+	
+	
+	// for(let across = 1; across < 130; across++){
+	// 	noiseRed = getNoiseValue(spacingSize*across, 100, 0.8, "noiseRed",0,1, 200 )
+	// 	noiseRedLerp = lerpColor(mainRed,backupRed,noiseRed)  // https://p5js.org/reference/#/p5/lerpColor
+	
+	// 	fill(noiseRedLerp);
+	// 	noStroke();
+	// 	rectMode(CENTER);
+	// 	rect(200, 100, 130, 100);
+	// }
 
+	// let blockSize = 10;
+	// let minSize = 1;
+	// let maxSize = 100;
+	// let rectX = 200;
+	// let rectY = 100;
 
+	// fill(mainRed);
+	// noStroke();
+	// let scaledSize = map(blockSize, minSize, maxSize, 0.1, 5);
+	// scale(scaledSize);
+	// rect(rectX, rectY, 130, 100);
+	// blockSize = (blockSize % maxSize) + 1;
+
+}
